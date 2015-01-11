78e48c2f59f67f8dfe8999a72fbe325e5a81a318
Frank <morfog371@gmail.com>
2015-01-11 13:09:01 +0100

    jQuery Fix
    Fix Thumb preview

--- assets/js/gallery_creator_fe.js
+++ assets/js/gallery_creator_fe.js
@@ -8,61 +8,77 @@
 
 // Dollar Safe Mode
 (function ($) {
-    window.addEvent('domready', function () {
+		if (typeof(window.event) != "undefined")
+		{
+			window.attachEvent("onDOMContentLoaded", function () {
         //Create the global GalleryCreatorFe-object
         objGalleryCreator = new GalleryCreatorFe();
-    });
+    	});
+		}
+		else
+		{
+    	window.addEventListener('DOMContentLoaded', function () {
+        //Create the global GalleryCreatorFe-object
+        objGalleryCreator = new GalleryCreatorFe();
+    	});
+		}
 
-
-    GalleryCreatorFe = new Class({
+		GalleryCreatorFe = function(){
+			var s_self = this;
+		};
+    GalleryCreatorFe.prototype = {
+				s_self : this,
         initialize: function () {
             //constructor
-            this.thumbOpacity = 1;
+            s_self.thumbOpacity = 1;
         },
 
         initThumbSlide: function (el, albumId, countPictures) {
             var self = this;
            
             //set some class-vars
-            this.currentDiv = document.id(el);
-            this.thumb = document.id(el).getElement('img.thumb');
-            this.albumId = albumId;
-            this.countPictures = countPictures;
-            this.currentPic = 0;
-            this.defaultThumbSrc = this.thumb.getProperty('src');
+            this.s_self.currentDiv = jQuery(el);
+            this.s_self.thumb = this.s_self.currentDiv.find('img.thumb');
+            this.s_self.albumId = albumId;
+            this.s_self.countPictures = countPictures;
+            this.s_self.currentPic = 0;
+            this.s_self.defaultThumbSrc = this.s_self.thumb.attr('src');
             var currentTime = new Date();
-            this.eventId = currentTime.getTime();
-            this.lastSlide = currentTime.getTime();
+            this.s_self.eventId = currentTime.getTime();
+            this.s_self.lastSlide = currentTime.getTime();
             //add the onmouseout-event
-            this.currentDiv.addEvent('mouseout', function () {
+            this.s_self.currentDiv.bind('mouseout', function () {
                 self.stopThumbSlide();
             });
 
             //slide thumbs after a delay of xxx milliseconds
-            this.startThumbSlide(this.eventId);
+            this.startThumbSlide(this.s_self.eventId);
         },
 
         stopThumbSlide: function () {
-            this.eventId = null;
-            if (this.thumb.getProperty('src') != this.defaultThumbSrc) {
-                this.thumb.fade(this.thumbOpacity);
-                this.thumb.set('opacity', this.thumbOpacity);
-                this.thumb.setProperty('src', this.defaultThumbSrc);
+            this.s_self.eventId = null;
+            if (this.s_self.thumb.attr('src') != this.s_self.defaultThumbSrc) {
+                this.s_self.thumb.fadeTo(this.s_self.thumbOpacity);
+                this.s_self.thumb.attr('src', this.s_self.defaultThumbSrc);
             }
-            this.thumb.fade(this.thumbOpacity);
+            this.s_self.thumb.fadeTo(this.s_self.thumbOpacity);
         },
 
         startThumbSlide: function (eventId) {
             var self = this;
-            var myRequest = new Request.JSON({
+            //next pic
+            this.s_self.currentPic++;
+            var myRequest = new jQuery.ajax({
                 url: document.URL,
-                method: 'get',
+                type: 'get',
+								dataType: "json",
+								data: 'isAjax=1&thumbSlider=1&AlbumId=' + this.s_self.albumId + '&limit=' + this.s_self.currentPic + '&eventId=' + eventId,
 
-                onSuccess: function (responseText) {
+                success: function (responseText) {
                     if (!responseText) return;
-                    if (responseText.eventId != self.eventId) return;
-                    if (responseText.eventId == null || self.eventId == null) return;
-                    if (responseText.thumbPath != "" && responseText.thumbPath != self.thumb.getProperty('src')) {
+                    if (responseText.eventId != self.s_self.eventId) return;
+                    if (responseText.eventId == null || self.s_self.eventId == null) return;
+                    if (responseText.thumbPath != "" && responseText.thumbPath != self.s_self.thumb.attr('src')) {
                         var currentTime = new Date();
                         if (currentTime.getTime() - self.lastSlide < 1200) {
                             self.startThumbSlide(eventId);
@@ -70,22 +86,17 @@
                         }
 
                         self.lastSlide = currentTime.getTime();
-                        var thumb = self.thumb;
-                        thumb.setProperty('src', responseText.thumbPath);
+                        var thumb = self.s_self.thumb;
+                        thumb.attr('src', responseText.thumbPath);
 
                     }
-
                     self.startThumbSlide(responseText.eventId);
-
                 }
             });
             if (self.currentPic == self.countPictures - 1) {
                 self.currentPic = 0;
             }
-            //next pic
-            self.currentPic++;
-            myRequest.send('isAjax=1&thumbSlider=1&AlbumId=' + self.albumId + '&limit=' + self.currentPic + '&eventId=' + eventId);
+            //myRequest.ajaxSend();
         }
-    });
+    };
 })(document.id);
-
